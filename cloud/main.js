e29f54a8b36eae009dfd9375e55661917f6c48b2
Alex Chen <chena@cfa.vic.gov.au>
2016-02-23 16:55:05 +1100

    updated main.js for beforeSave trigger testing

--- cloud/main.js
+++ cloud/main.js
@@ -456,6 +456,7 @@ Parse.Cloud.beforeSave("GCUR_LOCATION", function(request, response) {
  * Populate all ShareBy{STATE} columns available by "True" beforeSave a new Observation is added
  */
 Parse.Cloud.beforeSave("GCUR_OBSERVATION", function(request, response) {
+    console.log("beforeSave called on GCUR_OBSERVATION");
     Parse.Cloud.useMasterKey();
     sharedWithJurisArr = [];
      
