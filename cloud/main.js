f5a6d442b6d4172623a0feab5f1006a188204788
Alex Chen <grasslandcuring@gmail.com>
2020-08-09 22:39:44 +1000

    updated isLocationNameExist to Parse Server 3+

--- cloud/main.js
+++ cloud/main.js
@@ -386,18 +386,17 @@ Parse.Cloud.define("countOfObservations", async (request) => {
 	return countOfObs;
 });
 
-Parse.Cloud.define("isLocationNameExist", function(request, response) {
-	var query = new Parse.Query("GCUR_LOCATION");
+Parse.Cloud.define("isLocationNameExist", async (request) => {
+	const query = new Parse.Query("GCUR_LOCATION");
     query.equalTo("LocationName", request.params.locationName);
     query.limit(1000);
-    query.find().then(function(results) {
-    	if (results.length > 0)
-    		response.success(results[0]);
-    	else
-    		response.success(new Object());
-    }, function(error) {
-    	response.error("GCUR_LOCATION table lookup failed");
-    });
+    const results = await query.find(); {
+	
+	if (results.length > 0)
+    	return results[0];
+    else
+    	return new Object();
+    }
 });
 
 Parse.Cloud.define("deleteUserByUsername", function(request, response) {
