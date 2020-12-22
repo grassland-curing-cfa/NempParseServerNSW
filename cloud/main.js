7875257d356190e5bf259b60dd08537cac481263
Alex Chen <chena@cfa.vic.gov.au>
2020-12-22 16:40:02 +1100

    test

--- cloud/main.js
+++ cloud/main.js
@@ -47,7 +47,7 @@ var SUPERUSER_OBJECTID = "AucN1rSA60";
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", (request) => {
-	return Promise.resolve("Hello world from " + process.env.APP_NAME);
+	return Promise.resolve("Hello world from  " + process.env.APP_NAME);
 });
  
 Parse.Cloud.define("getDateInAEST", (request) => {
